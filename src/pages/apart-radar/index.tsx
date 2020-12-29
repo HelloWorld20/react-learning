c5ab0bf1ddec020abb0f8fb35caa9ff1025da7e4
Leon.W <292701515@qq.com>
2020-12-29 19:31:21 +0800

    'commit'

--- src/pages/apart-radar/index.tsx
+++ src/pages/apart-radar/index.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
 import { EChartOption, ECharts, init } from "echarts";
+import { Table } from "antd";
 
 import datas from "./data/index";
 
+import "antd/dist/antd.css";
 import "./index.css";
 
-type keyOfApartScore = keyof ApartScoreOption;
-
 export default () => {
+  const [tableData, setTabelData] = useState<any[]>([]);
   const chartEl = useRef<HTMLDivElement | null>(null);
   const instance = useRef<ECharts | null>(null);
 
@@ -59,8 +60,58 @@ export default () => {
       instance.current = init(chartEl.current);
 
       instance.current.setOption(getOption());
+
+      instance.current.on("legendselected", (e: any) => {
+        console.log("e)", e);
+        // handleSelectChange()
+      });
+
+      initTable();
     }
   }, []);
+
+  const initTable = () => {
+    const res: any[] = datas.map((data) => ({
+      name: data.name,
+      zone: `${data.zone}/${data.district}`,
+      prise: data.prise,
+      advantage: data.advantage,
+      disadvatage: data.disadvatage,
+    }));
+    setTabelData(res);
+  };
+
+  const handleSelectChange = (selected: string[]) => {
+    console.log("selected", selected);
+  };
+
+  const columns = [
+    {
+      title: "姓名",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "行政区",
+      dataIndex: "zone",
+      key: "zone",
+    },
+    {
+      title: "均价",
+      dataIndex: "prise",
+      key: "prise",
+    },
+    {
+      title: "优点",
+      dataIndex: "advantage",
+      key: "advantage",
+    },
+    {
+      title: "缺点",
+      dataIndex: "disadvatage",
+      key: "disadvatage",
+    },
+  ];
 
   return (
     <div className="apart-radar">
@@ -70,6 +121,7 @@ export default () => {
           chartEl.current = el;
         }}
       ></div>
+      <Table dataSource={tableData} columns={columns} />;
     </div>
   );
 };
